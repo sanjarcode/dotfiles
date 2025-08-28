df615691f1731a1dae9f8cf7f3ef9c54740ddb94
Sanjar Afaq <sanjarcode@gmail.com>
2025-08-28 22:04:48 +0530

    simplify

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 cp ~/.dotfiles/.zshrc ~/.dotfiles/.terminal_tool.sh ~/.dotfiles/.path_declarations.sh ~/.dotfiles/.function_declarations.sh ~/.dotfiles/.invocations.sh ~
 echo "\nDot files installed"
 source  ~/.zshrc
+dotfiles_check
 echo "\nDot files initialized"
+
